7f15c49321d6952955e97396d0d01b1f9bcf00d4
anoopanni <anoop.shivayogi@gmail.com>
2023-02-15 21:49:32 -0800

    Modified lot of things

---
+++ src/resumeData.js
@@ -0,0 +1,105 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Anoop Shivayogi",
+    "role": "Software Development Engineer",
+    "linkedinId":"https://www.linkedin.com/in/anoop-shivayogi/",
+    "roleDescription": " I'm truly passionate about discovering clever and simple solutions to complex problems. Demonstrated experience in the IT industry as a full-stack developer with strong skills in web development, and DevOps. I'm highly skilled in python and cloud technologies.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/anoop-shivayogi/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/anoopanni",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/anoopshivu",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Santa Clara, California, USA",
+    "website":"",
+    "education":[
+      {
+        "UniversityName":"Santa Clara University",
+        "specialization":"Master of Science, Computer Science and Engineering",
+        "MonthOfPassing":"Mar",
+        "YearOfPassing":"2024",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Bangalore Institute of Technology",
+        "specialization":"Bachelor of Engineering, Computer Science and Engineering",
+        "MonthOfPassing":"Mar",
+        "YearOfPassing":"2019",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
